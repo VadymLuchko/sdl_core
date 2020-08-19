172b60ca1040417cefa8b300371afa25f091aa40
sniukalov <sniukaov@luxoft.com>
2020-08-19 22:04:47 +0300

    Add timeout processing for resumption rc get interior vehicle data

--- src/components/application_manager/rpc_plugins/rc_rpc_plugin/src/commands/hmi/rc_get_interior_vehicle_data_request.cc
+++ src/components/application_manager/rpc_plugins/rc_rpc_plugin/src/commands/hmi/rc_get_interior_vehicle_data_request.cc
@@ -31,6 +31,7 @@
  */
 
 #include "rc_rpc_plugin/commands/hmi/rc_get_interior_vehicle_data_request.h"
+#include "application_manager/message_helper.h"
 #include "application_manager/resumption/resume_ctrl.h"
 
 #include "utils/macro.h"
@@ -55,11 +56,20 @@ void RCGetInteriorVehicleDataRequest::Run() {
 }
 
 void RCGetInteriorVehicleDataRequest::onTimeOut() {
-  auto& resume_ctrl = application_manager_.resume_controller();
+  LOG4CXX_TRACE(logger_,
+                "function_id: " << function_id()
+                                << " correlation_id: " << correlation_id());
+  using namespace application_manager;
+  event_engine::Event timeout_event(
+      hmi_apis::FunctionID::RC_GetInteriorVehicleData);
 
-  resume_ctrl.HandleOnTimeOut(
+  auto error_response = MessageHelper::CreateNegativeResponseFromHmi(
+      function_id(),
       correlation_id(),
-      static_cast<hmi_apis::FunctionID::eType>(function_id()));
+      hmi_apis::Common_Result::GENERIC_ERROR,
+      std::string("Timed out"));
+  timeout_event.set_smart_object(*error_response);
+  timeout_event.raise(application_manager_.event_dispatcher());
 }
 
 }  // namespace commands
